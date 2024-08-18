e3c790c97df93e181b0a1103fb98c05ba4862992
WhatDoYouKnow <max@xammap.com>
2024-08-18 22:08:55 +0100

    Create defines.h

---
+++ defines.h
@@ -0,0 +1,32 @@
+#define SCORELINE   63    // starts at 0! 63-24 to give starting row to reach bottom of display
+#define NAMELINE    37
+#define GAMESLINE   12
+#define GAMESSCORELINE   26
+#define UNDERTITLE  27
+#define MESSAGELINE 33
+#define DIGIT1      0
+#define DIGIT2      16
+#define COLON       31
+#define DIGIT3      35
+#define DIGIT4      50
+
+// #define AIO_RED_SCORE   "red-score"
+// #define AIO_RED_NAME    "redname"
+// #define AIO_RED_SERVE   "scores-group.red-serve-change"
+// #define AIO_BLUE_SCORE  "blue-score"
+// #define AIO_BLUE_NAME   "bluename"
+// #define AIO_BLUE_SERVE  "scores-group.blue-serve-change"
+
+#define LEFT            true
+#define RIGHT           false
+
+#define CYAN   0x03ff                   //0000 0011 1111 1111
+#define RED    0xf800                   //1111 1000 0000 0000
+#define WHITE  0xffff                   //1111 1111 1111 1111
+#define PURPLE 0xf81f                   //1111 1000 0001 1111
+#define GREEN  0x07e0                   //0000 0111 1110 0000
+#define BLUE   0x001f                   //0000 0000 0001 1111
+#define YELLOW 0xffc0                   //1111 1111 1100 0000
+#define BLACK  0
+
+#define ONLINEMESSAGE "Online"
