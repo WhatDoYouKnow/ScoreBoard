0c32b181cb162b6bb14c07430471e66117ce88a4
WhatDoYouKnow <max@xammap.com>
2024-08-18 22:06:55 +0100

    Create ElectricTin_SSL_h.ino
    First working version

---
+++ ElectricTin_SSL_h.ino
@@ -0,0 +1,470 @@
+/* ----------------------------------------------------------------------
+
+ElectricTin_SSL_h is intended to use MQTT over the Matrix Portal S3 or M4 WiFi
+to talk to HiveMQ broker.
+
+Includes:
+ArduinoMqttClient.h + dependencies
+
+  WiFiNINA.h      for MPM4
+OR
+  WiFi.h          for MPS3
+  ESP_SSLCient.h
+
+Adafruit_Protomatter.h + dependencies
+
+passwords.h   // this contains the "secret" information required to get online with MQTT and local WiFi
+              // written specifically for HiveMQ with SSL but should be easily ported for other brokers
+defines.h     // contains the other #define statements
+
+Extra files:
+matrix-routines.ino         routines for displaying data on the LED display
+connection-routines.ino     routines for connection to various online services
+----------------------------------------------------------------------- */
+
+#define DEBUG            // for most debug serial prints
+//#define DEBUG_BUFFER     // serial.print circular buffer top/tail
+// uncomment this line for a Matrix Portal S3 / comment if using M4
+#define MPS3             // MatrixPortal ESP32-S3
+// uncomment this line for a Matrix Portal M4 / comment if using S3
+// #define MPM4            // MatrixPortal M4
+#define P64X32           // 2 x 64x32 panels
+#define P64X64           // 1 x 64x64 panel
+
+// this #define is required for M4
+#ifdef MPM4
+  #define USE_AIRLIFT
+#endif
+
+// GNU C-compiler macros for filenames and versions
+#define PROGRAM_NAME      __BASE_FILE__
+#define PROGRAM_DATE      __DATE__
+#define PROGRAM_VERSION   "h_0.0.7" // now copes with 64x32 panels
+
+// protomattier include
+#include <Adafruit_Protomatter.h>
+
+
+#ifdef MPM4
+  #include <WiFiNINA.h>         // needed for M4 compilation
+  #pragma message "Compiling for Matrix Portal M4";
+#elif defined MPS3
+  #include <WiFi.h>             // needed for S3 compilation
+  #include <ESP_SSLClient.h>    // needed for S3 compilation
+  #pragma message "Compiling for Matrix Portal S3";
+#else
+  #error No #define for board - stopping compilation
+#endif
+
+#include <ArduinoMqttClient.h>        // mqtt library
+#include "passwords.h"                // store your passwords here
+#include "defines.h"                  // other definitions
+
+// these initialisations are different depending on the board
+#ifdef MPM4
+  WiFiSSLClient wifiClient;    // needed for M4 compilation
+#elif defined MPS3
+  ESP_SSLClient sslClient;    // needed for S3 compilation
+  WiFiClient basicClient;
+#endif
+
+// initialise mqtt client
+MqttClient mqttClient(sslClient);
+
+
+// fonts for Protomatter/Adafruit GFX library
+// commented fonts are not used
+#include <Fonts/FreeSansBold18pt7b.h> // Large friendly font
+//#include <Fonts/FreeSansBoldOblique18pt7b.h>
+//#include <Fonts/FreeSansBoldOblique12pt7b.h>
+#include <Fonts/FreeSansBoldOblique9pt7b.h>
+#include <Fonts/FreeSerifBoldItalic18pt7b.h>
+#include <Fonts/FreeSans9pt7b.h>
+//#include <Fonts/FreeSansBold12pt7b.h>
+
+// specially designed for this application
+#include <Fonts/FreeSansBoldNarrow18pt7b.h>   // big font for points score
+//#include <Fonts/FreeSerifBoldItalic12pt7b.h>
+//#include <Fonts/FreeSerifBoldItalic9pt7b.h>
+//#include <Fonts/FreeSerifBoldItalic8pt7b.h>
+//#include <Fonts/FreeMono9pt7b.h>
+//#include <Fonts/FreeSerif9pt7b.h>
+#include <Fonts/TomThumb.h>         // message font
+//#include <Fonts/Picopixel.h>
+
+// specially designed for this application
+#include <Fonts/FreeMax9pt7b.h>     // 
+//#include <Fonts/Font5x5Fixed.h>
+//#include <Fonts/Font5x7Fixed.h>
+
+// specially designed for this application
+#include <Fonts/MaxSans9pt7b.h>     // better numbers for games score
+
+/* ----------------------------------------------------------------------
+The RGB matrix must be wired to VERY SPECIFIC pins, different for each
+microcontroller board. This first section sets that up for a number of
+supported boards.
+------------------------------------------------------------------------- */
+#if defined(MPM4)       // MatrixPortal M4
+  uint8_t rgbPins[]  = {7, 8, 9, 10, 11, 12};
+  uint8_t addrPins[] = {17, 18, 19, 20, 21};
+  uint8_t clockPin   = 14;
+  uint8_t latchPin   = 15;
+  uint8_t oePin      = 16;
+#elif defined(MPS3)     // MatrixPortal ESP32-S3
+  uint8_t rgbPins[]  = {42, 41, 40, 38, 39, 37};
+  uint8_t addrPins[] = {45, 36, 48, 35, 21};
+  uint8_t clockPin   = 2;
+  uint8_t latchPin   = 47;
+  uint8_t oePin      = 14;
+#endif
+
+
+/* ----------------------------------------------------------------------
+This is the initialisation code for the Protomatter matrix
+Depending on the size of the matrices, edit the numbers 
+Don't forget that the E-line needs to be soldered across 
+  on the Matrix Portal if a 64x64 matrix is used
+Also don't forget that the pin on the LED matrix might be 
+  8 or 16 - this is important for which pin to solder
+------------------------------------------------------------------------- */
+
+#ifdef P64X32
+Adafruit_Protomatter matrix(
+  64,                          // Matrix width in pixels
+  3,                           // Bit depth -- 6 here provides maximum color options / 3 may reduce artefacts with reduced colours available
+  1, rgbPins,                  // # of matrix chains, array of 6 RGB pins for each
+  4, addrPins,                 // # of address pins (height is inferred), array of pins *** this is for 64x32***
+  //5, addrPins,               // # of address pins (height is inferred), array of pins ***this is for 64x64***
+  clockPin, latchPin, oePin,   // Other matrix control pins
+  false,                       // TRUE = DOUBLE-BUFFERING / FALSE = NO BUFFERING. Not used for this program
+  2                            // 2 = 2 x 64x32 matrices one above the other, neither rotated
+  );                           // -2 = 2 x 64x32 matrices one above the other, one rotated 180 degrees
+#elif defined P64X64
+Adafruit_Protomatter matrix(
+  64,                          // Matrix width in pixels
+  3,                           // Bit depth -- 6 here provides maximum color options
+  1, rgbPins,                  // # of matrix chains, array of 6 RGB pins for each
+  //4, addrPins,               // # of address pins (height is inferred), array of pins *** this is for 64x32***
+  5, addrPins,                 // # of address pins (height is inferred), array of pins ***this is for 64x64***
+  clockPin, latchPin, oePin,   // Other matrix control pins
+  false                        // TRUE = DOUBLE-BUFFERING / FALSE = NO BUFFERING. Not used for this program
+  //,2        // apparently, +2 = 2 x 64x32 matrices one above the other, straight
+  );             //             -2 = 2 x 64 x 32 matrices one above the other, rotated 180 degrees
+#endif
+// Sundry globals used for animation ---------------------------------------
+
+int16_t  textX;        // Current text position (X)
+int16_t  textY;        // Current text position (Y)
+int16_t  textMin;      // Text pos. (X) when scrolled off left edge
+
+uint8_t  p1Score;
+bool     p1NameChange;
+bool     p1ScoreUpdate;
+
+uint8_t  p2Score;
+bool     p2NameChange;
+bool     p2ScoreUpdate;
+
+bool     mqttUpdate;
+
+char     p1Name[10] = {"PLAY1"};   // red player
+char     p2Name[10] = {"PLAY2"};  // blue player
+
+uint8_t  p1Games;
+uint8_t  p2Games;
+
+int8_t    mqttErrorString;
+uint8_t   currentMessage = 0;
+uint8_t   receivedMessage = 0;
+
+uint16_t   timerCheck;                // apparently, 60 seconds is the default - needs to be a uint16 rather than uint8
+
+char      message[10][30];
+char      messageHeader[10][15];
+char      messagePayload[10][15];
+
+
+void setup(void) 
+{
+  uint8_t serialCount;
+  uint8_t loopCount;
+  char    buff[200];
+
+  serialStart();
+  wifiStart();
+
+  delay(2000);
+  mqttStart();
+
+}
+
+void loop(void)
+{
+  uint8_t count_up;
+  uint8_t count_down;
+  uint8_t loopCount;
+
+  char player1[10];
+  char player2[10];
+
+  char buff[30];
+
+  bool lockMQTT = false;
+
+  uint8_t tempScore1;
+  uint8_t tempScore2;
+
+  flashMessage();
+
+  //write_big_title();
+  //delay(5000);
+
+  startSubscription();
+
+  delay(5000);  
+
+  clearScreen();
+
+  writeGamesTitle();
+  writeGamesScore();
+  writeP1Name(p1Name, LEFT);
+  writeP2Name(p2Name, RIGHT);
+  writeScoreDigit(p1Score, LEFT, RED);
+  writeScoreDigit(p2Score, RIGHT, BLUE);
+
+  // this section processes messages with a keepalive of slightly less than 60s
+  // due to the nature of potentially successive messages, process even when not sending keepalive
+  while(1)
+  {
+    if(timerCheck > 5950)         // given the default 60s timeout, allow some delay processing to ping before 60s is up
+    {
+      mqttClient.poll();          // refresh connection
+      timerCheck = 0;             // reset timerCheck
+    }
+    else
+    {
+      timerCheck++;               // timerCheck controls MQTT ping - once a minute
+    }
+
+    if(mqttUpdate)                // if we have a message to process
+    {
+      splitMessage();
+      Serial.print("currentMessage: ");Serial.println(currentMessage);
+      Serial.print("receivedMessage: ");Serial.println(receivedMessage);
+      Serial.print(messageHeader[currentMessage]); Serial.print(":"); Serial.println(messagePayload[currentMessage]); 
+
+      if(strcmp(messageHeader[currentMessage], "p1Name") == 0)
+      {
+        strcpy(p1Name, messagePayload[currentMessage]);
+        clearNameLeft();
+        writeP1Name(messagePayload[currentMessage], LEFT);
+        Serial.println("p1Name change");
+      }
+      else
+      {
+        if(strcmp(messageHeader[currentMessage], "p2Name") == 0)
+        {
+          strcpy(p2Name, messagePayload[currentMessage]);
+          clearNameRight();
+          writeP2Name(messagePayload[currentMessage], RIGHT);
+          Serial.print("p2Name change");
+        }
+        else
+        {
+          if(strcmp(messageHeader[currentMessage], "Games") == 0)
+          {
+            Serial.println("Games");
+            getGames();
+            refreshDisplay();
+          }
+          else
+          {
+            if(strcmp(messageHeader[currentMessage], "Score") == 0)
+            {
+              getScores();
+              Serial.println("Current game score");
+              clearScore();
+              writeScoreDigit(p1Score, LEFT, RED);
+              writeScoreColon();
+              writeScoreDigit(p2Score, RIGHT, BLUE);
+            }
+            else
+            {
+              if(strcmp(messageHeader[currentMessage], "Win") == 0)
+              {
+                playerWin();
+              }
+              else
+              {
+                if(strcmp(messageHeader[currentMessage], "Match") == 0)
+                {
+                  playerMatch();
+                }
+                else
+                {
+                  if(strcmp(messageHeader[currentMessage], "Next") == 0)
+                  {
+                    if(strcmp(messagePayload[currentMessage], "Match") == 0)
+                    {
+                      nextMatch();  
+                    }
+                    else
+                    {
+                      if(strcmp(messagePayload[currentMessage], "Game") == 0)
+                      {
+                        nextGame();
+                      }
+                      else
+                      {
+                        Serial.println("Error in NextGame logic");
+                      }
+                    }
+                  }
+                  else
+                  {
+                    Serial.println("Error in NextGame/Match logic");
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+
+      if(currentMessage < 9)                            // currentMessage/receivedMessage act as head/tail for a circular buffer of 10 messages
+        currentMessage++;
+      else
+        currentMessage = 0;
+
+      if(receivedMessage == currentMessage)             // if no new messages, stop checking
+        mqttUpdate = false;
+      
+      for(count_up=0;count_up<10;count_up++)            // print the message queue - neatly
+      {
+        sprintf(buff, "Mess%d: %s_%s\n", count_up, messageHeader[count_up], messagePayload[count_up]);
+        Serial.print(buff);
+      }
+    }
+    #ifdef DEBUG_BUFFER
+      Serial.println(currentMessage);
+      Serial.println(receivedMessage);
+    #endif
+  }
+  delay(10);
+}
+
+/*
+* onMqttMessage is the callback for receiving an MQTT message
+* int p_messaageSize is the length of the message
+* 
+*/
+
+void onMqttMessage(int p_messageSize)
+{
+  uint8_t parseMessage;
+
+  // got a message of size p_messageSize
+  #ifdef DEBUG
+    Serial.print("Received a message with topic: '");
+    Serial.print(mqttClient.messageTopic());
+    Serial.print("', Length: ");Serial.print(p_messageSize);Serial.println(" bytes:");
+  #endif
+
+  // apparently, we have to get the message char by char
+  // use the Stream interface to print the contents
+
+  parseMessage = 0;
+  while(mqttClient.available())
+  {
+    message[receivedMessage][parseMessage] = mqttClient.read();
+    Serial.print(message[receivedMessage][parseMessage]);
+    parseMessage++;
+  }
+  message[receivedMessage][parseMessage] = '\0';     // terminate string
+  Serial.print("\n\n");
+
+  if(receivedMessage < 9)
+    receivedMessage++;
+  else
+    receivedMessage = 0;
+
+  mqttUpdate = true;
+}
+
+/* void splitMessage(void)
+* Splits the message based on ':'
+* stores the split message in two char arrays:
+* messageHeader[]
+* messagePayload[]
+*
+*/
+void splitMessage(void)
+{
+  char localMessage[30];
+  char * localMessageHeader;    // used to pacify strtok
+  char * localMessagePayload;
+
+  Serial.print("message[currentMessage]: ");Serial.println(message[currentMessage]);
+  strcpy(localMessage, message[currentMessage]);
+  Serial.print("localMessage (splitMessage): "); Serial.println(localMessage);
+  localMessageHeader = strtok(localMessage, ":");
+  strcpy(messageHeader[currentMessage], localMessageHeader);
+  Serial.print("Header: "); Serial.println(messageHeader[currentMessage]);
+  localMessagePayload = strtok(NULL, ":");
+  strcpy(messagePayload[currentMessage], localMessagePayload);
+  Serial.print("Payload: "); Serial.println(messagePayload[currentMessage]);
+}
+
+
+/* void getScores(void)
+* Splits the message based on '-'
+* stores the original message in localP1Score[] and localP2Score[]
+* converts the scores into global uint8_t p1Score and p2Score
+*
+*/
+void getScores(void)
+{
+  char * localP1Score;      // local char array to store P1 score as chars
+  char * localP2Score;      // local char array to store P2 score as chars
+  char localScore[15];      // copy of message to prevent strtok destroying the message
+
+  strcpy(localScore, messagePayload[currentMessage]);   // create the copy
+  localP1Score = strtok(localScore, "-");               // get first score in chars
+  localP2Score = strtok(NULL, "-");                     // get second score in chars
+
+  p1Score = atoi(localP1Score);                         // convert to global uint8_t
+  p2Score = atoi(localP2Score);                         // convert to global uint8_t
+
+  #ifdef DEBUG                                               // if DEBUG set
+    Serial.print("Score P1: "); Serial.println(p1Score);  // print the score
+    Serial.print("Score P2: "); Serial.println(p2Score);  // print the score
+  #endif
+}
+
+/* void getGames(void)
+* Splits the message based on '-'
+* stores the original message in localScore[]
+* converts the scores into global uint8_t p1Score and p2Score
+*
+*/
+
+void getGames(void)
+{
+  char * localP1Games;                // char array of P1Games
+  char * localP2Games;                // char array of P2Games
+  char localGames[15];                // copy of original message to prevent destruction
+
+  strcpy(localGames, messagePayload[currentMessage]);   // create the copy
+  localP1Games = strtok(localGames, "-");               // get first games in chars
+  localP2Games = strtok(NULL, "-");                     // get second games in chars
+
+  p1Games = atoi(localP1Games);                         // convert to global uint8_t
+  p2Games = atoi(localP2Games);                         // convert to global uint8_t
+
+  #ifdef DEBUG                                             // if DEBUG set
+    Serial.print("Score P1: "); Serial.println(p1Games);// print the games
+    Serial.print("Score P2: "); Serial.println(p2Games);// print the games
+  #endif
+}
+
